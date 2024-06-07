42c2fde150270e7e94f61355ad7faf362b1311ff
SeemaAggarwal <121787754+DkAggarwal2512@users.noreply.github.com>
2024-06-07 14:53:37 +0530

    Add files via upload

--- Pattren.java
+++ Pattren.java
@@ -1,21 +1,21 @@
 
 // 1.Rectangle Pattren
-// public class Pattren{
-//     public static void main(String args[]) {
-//         // Nested Loops
-//         int n= 4;
-//         int m=5;
-//         // outer loop
-//         for(int i=1; i<=n; i++){
-//             // inner loop
-//             for(int j=1; j<=m; j++){
-//                 System.out.print("*");
-//             }
-//             System.out.println();
-//         }
+public class Pattren{
+    public static void main(String args[]) {
+        // Nested Loops
+        int a= 4;
+        int b=5;
+        // outer loop
+        for(int i=1; i<=a; i++){
+            // inner loop
+            for(int j=1; j<=b; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
-//     }
-// }
+    
+    // }}
 
 
 
@@ -25,23 +25,23 @@
 // 2.Hollow Rectangle
 // public class Pattren{
 //     public static void main(String args[] ){
-//         int n = 4;
-//         int m =5;
-//         // outer loop
-//         for(int i=1; i<=n; i++){
-//             // inner loop
-//             for(int j=1; j<=m; j++){
-//                 // cell ->(i,j)
-//                 if(i==1 || j==1 || i==n || j==m){
-//                     System.out.print("#");
-//                     }
-//                     else{
-//                         System.out.print(" ");
-//                     }
-//             }
-//             System.out.println(); 
+        int c = 4;
+        int d =5;
+        // outer loop
+        for(int i=1; i<=c; i++){
+            // inner loop
+            for(int j=1; j<=d; j++){
+                // cell ->(i,j)
+                if(i==1 || j==1 || i==c || j==d){
+                    System.out.print("#");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+            }
+            System.out.println(); 
 
-//             }
+            }
 //     }
 // }
 
@@ -52,16 +52,16 @@
 // 3.Half Pyramid
 // public class Pattren{
 //     public static void main(String args[] ){
-//         int n = 4;
-//         // outer loop
-//         for(int i=1; i<=n; i++){
-//             // inner loop
-//             for(int j=1; j<=i; j++){
-//                 System.out.print("@");
-//             }
+        int e = 4;
+        // outer loop
+        for(int i=1; i<=e; i++){
+            // inner loop
+            for(int j=1; j<=i; j++){
+                System.out.print("@");
+            }
 
-//             System.out.println();
-//         }
+            System.out.println();
+        }
 
 //     }
 
@@ -69,44 +69,48 @@
 
 
 
+
+
 // 4. INVERTED Half Pyramid
 
 // public class Pattren{
 //         public static void main(String args[] ){
-//             int n = 4;
-//             // outer loop
-//             for(int i=n; i>=1; i--){
-//                 // inner loop
-//                 for(int j=1; j<=i; j++){
-//                     System.out.print("*");
-//                 }
+            int f = 4;
+            // outer loop
+            for(int i=f; i>=1; i--){
+                // inner loop
+                for(int j=1; j<=i; j++){
+                    System.out.print("*");
+                }
     
-//                 System.out.println();
-//             }
+                System.out.println();
+            }
     
 //         }
     
 //     }
+
+
     
 
 // 5.INVERTED Half Pyramid
 
 // public class Pattren{
 //             public static void main(String args[] ){
-//                 int n = 4;
-//                 // outer loop
-//                 for(int i=1; i<=n; i++){
-//                     // inner loop print Space 
-//                     for(int j=1; j<=n-i; j++){
-//                         System.out.print(" ");
-//                     }
+                int g = 4;
+                // outer loop
+                for(int i=1; i<=g; i++){
+                    // inner loop print Space 
+                    for(int j=1; j<=g-i; j++){
+                        System.out.print(" ");
+                    }
 
-//                     // inner Loop Print Star
-//                     for(int j=1; j<=i; j++){
-//                         System.out.print("*");
-//                     }        
-//                     System.out.println();
-//                 }        
+                    // inner Loop Print Star
+                    for(int j=1; j<=i; j++){
+                        System.out.print("*");
+                    }        
+                    System.out.println();
+                }        
 //             }        
 //         }
 
@@ -115,30 +119,19 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
         // public class Pattren{
         //     public static void main(String args[] ){
-        //         int n = 5;
-        //         // outer loop
-        //         for(int i=1; i<=n; i++){
-        //             // inner loop print Space 
-        //             for(int j=1; j<=i; j++){
-        //                 System.out.print(j+" ");
-        //             }
+                int h = 5;
+                // outer loop
+                for(int i=1; i<=h; i++){
+                    // inner loop print Space 
+                    for(int j=1; j<=i; j++){
+                        System.out.print(j+" ");
+                    }
 
                       
-        //             System.out.println();
-        //         }        
+                    System.out.println();
+                }        
         //     }        
         // }
 
@@ -147,17 +140,17 @@
 
 // public class Pattren{
 //         public static void main(String args[] ){
-//             int n = 5;
-//             // outer loop
-//             for(int i=1; i<=n; i++){
-//                 // inner loop print Space 
-//                 for(int j=1; j<=n-i+1; j++){
-//                     System.out.print(j);
-//                 }
+            int k = 5;
+            // outer loop
+            for(int i=1; i<=k; i++){
+                // inner loop print Space 
+                for(int j=1; j<=k-i+1; j++){
+                    System.out.print(j);
+                }
 
                   
-//                 System.out.println();
-//             }        
+                System.out.println();
+            }        
 //         }        
 //     }
 
@@ -167,30 +160,30 @@
     
     // public class Pattren{
     //             public static void main(String args[] ){
-    //                 int n = 5;
-    //                 int num = 1;
-    //                 // outer loop
-    //                 for(int i=1; i<=n; i++){
-    //                     // inner loop print Space 
-    //                     for(int j=1; j<=i; j++){
-    //                         System.out.print(num+" ");
-    //                         num++;
-    //                     }
+                    int l = 5;
+                    int lnum = 1;
+                    // outer loop
+                    for(int i=1; i<=l; i++){
+                        // inner loop print Space 
+                        for(int j=1; j<=i; j++){
+                            System.out.print(lnum+" ");
+                            lnum++;
+                        }
         
                           
-    //                     System.out.println();
-    //                 }        
+                        System.out.println();
+                    }        
     //             }        
     //         }
 
 
 // 9. 0-1 Triangle
-    public class Pattren{    
-    public static void main(String args[] ){
-        int n = 5;
+    // public class Pattren{    
+    // public static void main(String args[] ){
+        int m = 5;
 
         // outer loop
-        for(int i=1; i<=n; i++){
+        for(int i=1; i<=m; i++){
             // inner loop print Space 
             for(int j=1; j<=i; j++){
                 int sum = i+j;
@@ -203,8 +196,8 @@
             }
             System.out.println();
         }        
-    }        
-}
+          
+
 
 
         
@@ -212,6 +205,218 @@
 
 
 
+
+
+
+
+        // ButterFly
+
+
+        // public class Pattren {
+        //     public static void main(String[] args) {
+        
+        int o = 5;
+
+
+        // Upper Part
+        for(int i=1; i<=o; i++){
+            // 1st
+            for(int j=1; j<=i; j++){
+                System.out.print("*");
+            }
+
+            // Space
+            int spaces = 2 * (o-i);
+            for(int j=1; j<=spaces; j++){
+                System.out.print(" ");
+        }
+
+        // 2nd
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
+
+    }
+    // lower
+
+for(int i=o; i>=1; i--){
+
+        // 1st
+        for(int j=1; j<=i; j++){
+            System.out.print("*");
+        }
+
+        // Space
+        int spaces = 2 * (o-i);
+        for(int j=1; j<=spaces; j++){
+            System.out.print(" ");
+    }
+
+    // 2nd
+    for(int j=1; j<=i; j++){
+        System.out.print("*");
+    }
+    System.out.println();
+
+}
+            // }}
+ 
+
+
+
+
+
+
+
+
+
+// Rhombus
+// public class Pattren {
+//     /**
+//      * @param args
+//      */
+//     public static void main(String[] args) {
+
+int p = 5;
+
+
+
+
+
+for(int i=1; i<=p;i++){
+    // spaces
+    for(int j=1; j<=p-i; j++){
+        System.out.print(" ");
+    }
+// stars
+for(int j=1; j<=5; j++){
+    System.out.print("*");
+}
+System.out.println();
+
+}
+// }}
+
+
+
+
+
+
+
+
+//  Number Pyramid
+
+// public class Pattren {
+//     /**
+//      * @param args
+//      */
+//     public static void main(String[] args) {
+
+int q = 5;
+
+
+
+
+for(int i=1; i<=q; i++){
+    // spaces
+    for(int j=1; j<=q-i; j++){
+        System.out.print(" ");
+    }
+
+    // numbers -> print row no , row no times
+    for(int j=1; j<=i; j++){
+        System.out.print(i + " ");
+    }
+
+    System.out.println();
+}
+
+//     }}
+
+// Palindromic 
+
+
+// public class Pattren {
+//     /**
+//      * @param args
+//      */
+//     public static void main(String[] args) {
+
+int s = 5;
+
+
+for(int i=1; i<=s; i++){
+    // spaces
+    for(int j=1; j<=s-i; j++){
+        System.out.print(" ");
+    }
+
+    // numbers of 1st part
+    for(int j=i;  j>=1; j--){
+        System.out.print(j);
+    }
+
+    // 2nd Part
+    for(int j=2; j<=i; j++){
+        System.out.print(j);
+    }
+
+
+    System.out.println();
+}
+//     }}
+
+
+
+// Diamond       
+
+
+// public class Pattren {
+//     /**
+//      * @param args
+//      */
+//     public static void main(String[] args) {
+
+int t = 4;
+
+
+// Upper Part
+for(int i=1; i<=t; i++){    
+    // spaces
+    for(int j=1; j<=t-i; j++){
+        System.out.print(" ");
+    }
+    // stars
+    for(int j=1; j<=2*i-1; j++){
+        System.out.print("*");
+    }    
+    System.out.println();
+}
+//  Lower Part
+for(int i=t; i>=1; i--){    
+    // spaces
+    for(int j=1; j<=t-i; j++){
+        System.out.print(" ");
+    }
+    // stars
+    for(int j=1; j<=2*i-1; j++){
+        System.out.print("*");
+    }    
+    System.out.println();
+}
+
+
+
+// }}
+
+
+
+
+ 
+
+
+    }}
 
 
 
